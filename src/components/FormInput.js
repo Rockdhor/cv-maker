13fdba73a8e15e1bc7ae8af307cdf339de7c2f54
Allen Schmerler <rockdhor@gmail.com>
2022-06-07 21:07:28 -0400

    Created FormInput component.

---
+++ src/components/FormInput.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class FormInput extends Component {
+  render() {
+    return (
+      <div className={"form-group col-" + this.props.width}>
+          <label for={"input" + this.props.label}>
+          {this.props.label}
+          </label>
+          <input 
+          type="text" className='form-control'
+          id={"input" + this.props.label}
+          onChange={this.props.updateState} 
+          data-property={this.props.property} 
+          placeholder={this.props.label}
+          />
+      </div>
+    )
+  }
+}
